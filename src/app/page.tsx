0b10fedbbde609e1d56df8cab9c6bfe8d4264357
kimberli <kimberlizhong@gmail.com>
2024-03-28 17:22:20 +0300

    fix plant and table resizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
   return (
-    <main className="bg-gradient-to-r from-primary via-primary-light to-primary-dark flex flex-col items-center justify-between p-24 relative min-h-[600px] min-w-[600px] h-screen">
+    <main className="bg-gradient-to-r from-primary via-primary-light to-primary-dark flex flex-col items-center justify-between p-24 relative min-h-[600px] min-w-[600px] h-screen overflow-x-hidden">
       {/* Wall items consist of the photo array and the bookshelf. */}
       <div className="absolute flex flex-col gap-6 tall:gap-12 bottom-80 w-full">
         {/* Photos should be inline on short screens and alternating on tall screens */}
@@ -14,7 +14,7 @@ const Home: React.FC<HomeProps> = () => {
               src="/photo_a.svg"
               className="drop-shadow-[2px_2px_2px_rgba(0,0,0,0.4)]"
               alt="Photo A"
-              layout="fill"
+              fill={true}
               object-fit="contain"
               priority
             />
@@ -24,7 +24,7 @@ const Home: React.FC<HomeProps> = () => {
               src="/photo_b.svg"
               className="drop-shadow-[2px_2px_2px_rgba(0,0,0,0.4)]"
               alt="Photo B"
-              layout="fill"
+              fill={true}
               object-fit="contain"
               priority
             />
@@ -34,7 +34,7 @@ const Home: React.FC<HomeProps> = () => {
               src="/photo_c.svg"
               className="drop-shadow-[2px_2px_2px_rgba(0,0,0,0.4)]"
               alt="Photo C"
-              layout="fill"
+              fill={true}
               object-fit="contain"
               priority
             />
@@ -53,10 +53,9 @@ const Home: React.FC<HomeProps> = () => {
         </div>
         {/* Bike */}
         <div className="absolute flex flex-row items-center justify-center gap-6 bottom-[2rem] w-full">
-          <span className="w-[120px]" />
           <Image
             src="/bike.svg"
-            className="drop-shadow-[8px_8px_4px_rgba(0,0,0,0.2)]"
+            className="drop-shadow-[8px_8px_4px_rgba(0,0,0,0.2)] ml-[120px]"
             alt="Bike"
             width={100}
             height={80}
@@ -82,27 +81,30 @@ const Home: React.FC<HomeProps> = () => {
           priority
         />
       </div>
-      <div className="absolute bottom-14 hidden sm:flex flex-row items-end">
-        <Image
-          src="/flowers.svg"
-          className="drop-shadow-[8px_8px_8px_rgba(0,0,0,0.2)]"
-          alt="Flowers"
-          width={180}
-          height={200}
-          priority
-        />
-        <span className="sm:w-[600px] md:w-[700px]" />
+      {/* Don't display the side table when the screen is too narrow. */}
+      <div className="absolute bottom-14 hidden md:flex flex-row">
+        <div className="relative w-[180px] h-[200px] mr-[650px]">
+          <Image
+            src="/flowers.svg"
+            className="drop-shadow-[8px_8px_8px_rgba(0,0,0,0.2)] mr-[650px]"
+            alt="Flowers"
+            fill={true}
+            object-fit="contain"
+            priority
+          />
+        </div>
       </div>
-      <div className="absolute bottom-16 flex flex-row items-end justify-end">
-        <span className="sm:w-[600px] md:w-[700px]" />
-        <Image
-          src="/plant.svg"
-          className="drop-shadow-[16px_16px_16px_rgba(0,0,0,0.6)]"
-          alt="Plant"
-          width={250}
-          height={200}
-          priority
-        />
+      <div className="absolute bottom-16 flex flex-row items-center justify-center">
+        <div className="relative w-[250px] h-[400px] ml-[625px]">
+          <Image
+            src="/plant.svg"
+            className="drop-shadow-[16px_16px_16px_rgba(0,0,0,0.6)]"
+            alt="Plant"
+            fill={true}
+            object-fit="contain"
+            priority
+          />
+        </div>
       </div>
       <div className="absolute bottom-12 flex flex-row justify-center">
         <Image
