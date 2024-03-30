72d0dbb3f9bb90d262656afcb48c92625ca82009
kimberli <kimberlizhong@gmail.com>
2024-03-30 23:05:47 +0300

    add more pages

---
+++ src/app/photos/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import UnderConstruction from "@/components/UnderConstruction";
+
+const Page: React.FC = () => (
+  <main>
+    <UnderConstruction />
+  </main>
+);
+
+export default Page;
