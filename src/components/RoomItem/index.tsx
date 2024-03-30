1ae75f0383ae6966d6e3c0a31471688cb5b78a18
kimberli <kimberlizhong@gmail.com>
2024-03-30 04:31:32 +0300

    add ability to interact with elements

--- src/components/RoomItem/index.tsx
+++ src/components/RoomItem/index.tsx
@@ -1,11 +1,16 @@
+"use client";
+
+import classNames from "classnames";
 import Image from "next/image";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 interface RoomItemProps {
   src: string;
   className?: string;
   imageClass?: string;
   base64PlaceholderImage?: string; // Base64-encoded color to show while image is loading.
+  destination?: string;
 }
 
 const RoomItem: React.FC<RoomItemProps> = ({
@@ -13,9 +18,19 @@ const RoomItem: React.FC<RoomItemProps> = ({
   className,
   imageClass,
   base64PlaceholderImage,
+  destination,
 }) => {
+  const router = useRouter();
+
+  const classes = classNames(
+    className,
+    destination
+      ? "cursor-pointer z-10 hover:drop-shadow-[0_0_8px_rgba(255,220,115,1)]"
+      : "",
+  );
+
   return (
-    <div className={className}>
+    <div className={classes}>
       <Image
         src={src}
         className={imageClass}
@@ -23,6 +38,9 @@ const RoomItem: React.FC<RoomItemProps> = ({
         fill={true}
         priority
         placeholder={`data:image/png;base64,${base64PlaceholderImage}` || ""}
+        {...(destination && {
+          onClick: () => router.push(destination),
+        })}
       />
     </div>
   );
