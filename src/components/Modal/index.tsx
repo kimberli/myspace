8c0c5dd8e4b9e472e545cd3e7e42687452eac92c
kimberli <kimberlizhong@gmail.com>
2024-04-02 17:15:38 +0300

    adjust modal styling

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -19,15 +19,16 @@ const Modal: React.FC<ModalProps> = ({
         className="absolute bg-neutral-800/50 h-dvh w-full top-0 left-0 z-[100]"
         onClick={onClose}
       ></div>
-      <div className="absolute min-h-64 h-auto min-w-80 w-1/2 bg-neutral-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-8 rounded z-[200]">
+      <div className="absolute min-h-64 h-auto min-w-80 w-1/2 bg-neutral-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded z-[200]">
         <IconButton
-          className="float-right"
+          className="float-right m-4"
           onClick={onClose}
           icon={<HiOutlineXMark />}
         />
-
-        <h1 className="mb-4 text-center text-xl">{title}</h1>
-        {children}
+        <div className="p-8">
+          <h1 className="mb-4 text-center text-xl">{title}</h1>
+          {children}
+        </div>
       </div>
     </>
   );
