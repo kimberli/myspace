c4f7ccc9b2fc8191c0b8d16e4d3a08daa84b1198
kimberli <kimberlizhong@gmail.com>
2024-04-02 17:06:27 +0300

    add iconbutton

---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,33 @@
+import classNames from "classnames";
+import React from "react";
+
+interface IconButtonProps {
+  className?: string;
+  disabled?: boolean;
+  icon: React.ReactNode;
+  onClick: () => void;
+  outline?: boolean;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  className,
+  disabled,
+  icon,
+  onClick,
+  outline,
+}: IconButtonProps) => {
+  const defaultClasses = "p-0.5 rounded";
+  const classes = classNames(
+    defaultClasses,
+    disabled ? "cursor-default" : "cursor-pointer hover:bg-neutral-500/10",
+    outline && "border border-neutral-800",
+    className,
+  );
+  return (
+    <div className={classes} onClick={disabled ? undefined : onClick}>
+      {icon}
+    </div>
+  );
+};
+
+export default IconButton;
