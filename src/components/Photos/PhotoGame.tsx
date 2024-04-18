14c08e8591b3ae7ec64aea403e73dda085713c2d
kimberli <kimberlizhong@gmail.com>
2024-04-18 23:11:43 +0300

    correct game logic but with lint errors

--- src/components/Photos/PhotoGame.tsx
+++ src/components/Photos/PhotoGame.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { GameStateContext, GameStatus } from "@/context/GameState";
 import Button from "@/components/Button";
@@ -26,12 +26,21 @@ const PhotoGame: React.FC<PhotoGameProps> = ({
   isLoading,
 }: PhotoGameProps) => {
   const [currentPin, setCurrentPin] = useState<Pin | null>(null);
-  const [correctPin, setCorrectPin] = useState<Pin | null>(null);
+  const [currentImage, setCurrentImage] = useState<string>("");
   const { gameState, setGameStatus, addCompletedGuess } =
     useContext(GameStateContext);
 
-  let currentImage;
   let contents;
+  const completedGuess = gameState?.completedGuesses?.[currentImage];
+
+  const advanceCurrentImage = () => {
+    const imageIds = Object.keys(photoData);
+    const initialImage = imageIds.filter(
+      (imageId) => !gameState.completedGuesses.hasOwnProperty(imageId),
+    )[0];
+    setCurrentImage(initialImage);
+    setCurrentPin(null);
+  };
 
   const clickHandler = (e: MouseClickEvent): void => {
     setCurrentPin({
@@ -60,11 +69,6 @@ const PhotoGame: React.FC<PhotoGameProps> = ({
         throw new Error(`${response.status} ${response.statusText}`);
       }
       const result = await response.json();
-      setCorrectPin({
-        latitude: result.correctLatitude,
-        longitude: result.correctLongitude,
-        color: CORRECT_PIN_COLOR,
-      });
       addCompletedGuess(currentImage, {
         latitude: result.correctLatitude,
         longitude: result.correctLongitude,
@@ -76,6 +80,12 @@ const PhotoGame: React.FC<PhotoGameProps> = ({
     }
   };
 
+  useEffect(() => {
+    if (!isLoading) {
+      advanceCurrentImage();
+    }
+  }, [photoData, isLoading]);
+
   if (loadingError) {
     contents = (
       <p className="text-center">Error fetching . Please try again.</p>
@@ -83,15 +93,10 @@ const PhotoGame: React.FC<PhotoGameProps> = ({
   } else if (
     !isLoading &&
     photoData &&
-    Object.keys(photoData).length &&
+    currentImage &&
     gameState.completedGuesses
   ) {
-    const imageIds = Object.keys(photoData);
     // TODO(Kim): Only advance currentImage when user clicks a button.
-    currentImage = imageIds.filter(
-      (imageId) => !gameState.completedGuesses.hasOwnProperty(imageId),
-    )[0];
-    const completedGuess = gameState.completedGuesses[currentImage];
     const pins = [];
     if (currentPin) {
       pins.push(currentPin);
@@ -148,7 +153,11 @@ const PhotoGame: React.FC<PhotoGameProps> = ({
           outline
         />
         <div className="flex gap-2">
-          <Button onClick={makeGuess} text="Submit" />
+          {completedGuess ? (
+            <Button onClick={() => advanceCurrentImage()} text="Next" />
+          ) : (
+            <Button onClick={makeGuess} disabled={!currentPin} text="Submit" />
+          )}
         </div>
       </div>
     </div>
