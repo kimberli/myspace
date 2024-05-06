c94de22794c9509e01fed7ff180f44cb76afb32f
kimberli <kimberlizhong@gmail.com>
2024-05-06 15:40:08 -0500

    compute distance instead of score

--- src/app/api/photos/route.ts
+++ src/app/api/photos/route.ts
@@ -28,6 +28,7 @@ export type GuessResult = {
   score: number;
 };
 
+const EARTH_RADIUS_KM = 6371;
 const TYPED_PHOTO_DATA = PHOTO_DATA as RawPhotoData;
 
 const RESPONSE_DATA: ResponsePhotoData = Object.fromEntries(
@@ -36,6 +37,37 @@ const RESPONSE_DATA: ResponsePhotoData = Object.fromEntries(
     { blur: value.blur, label: value.label, description: value.description },
   ]),
 );
+
+const toRadians = (angle: number): number => {
+  return (angle * Math.PI) / 180;
+};
+
+const calculateDistance = (
+  guessLatitude: number,
+  guessLongitude: number,
+  correctLatitude,
+  correctLongitude: number,
+): number => {
+  // Estimate the distance between the two points using the Haversine formula.
+  const guessLatRad = toRadians(guessLatitude);
+  const guessLonRad = toRadians(guessLongitude);
+  const correctLatRad = toRadians(correctLatitude);
+  const correctLonRad = toRadians(correctLongitude);
+
+  const dLat = guessLatRad - correctLatRad;
+  const dLon = guessLonRad - correctLonRad;
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(correctLatRad) *
+      Math.cos(guessLatRad) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return EARTH_RADIUS_KM * c;
+};
 
 // TODO(Kim): Paginate this since it's reading a 3 MB file, maybe with swc/infinite?
 export function GET(): NextResponse<ResponsePhotoData | ErrorMessage> {
@@ -64,9 +96,11 @@ export async function POST(
     const correctLatitude = TYPED_PHOTO_DATA[photoId].lat as number;
     const correctLongitude = TYPED_PHOTO_DATA[photoId].lng as number;
     // TODO(Kim): Replace this with a real distance calculation.
-    const score = Math.sqrt(
-      (guessLatitude - correctLatitude) ** 2 +
-        (guessLongitude - correctLongitude) ** 2,
+    const score = calculateDistance(
+      guessLatitude,
+      guessLongitude,
+      correctLatitude,
+      correctLongitude,
     );
     const guessResult = {
       correctLatitude,
